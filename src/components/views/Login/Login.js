3690d09c14ca70bcccaf769e622fe3cea2b6b731
Malwina8 <48411653+Malwina8@users.noreply.github.com>
2020-04-11 18:26:13 +0200

    Add Login view using MUI

--- src/components/views/Login/Login.js
+++ src/components/views/Login/Login.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import styles from './Login.module.scss';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 class Login extends React.Component {
 
@@ -7,7 +9,16 @@ class Login extends React.Component {
 
     return (
       <div className={styles.component}>
-        <h2>LOGIN</h2>
+        <form className={styles.component}>
+          <TextField className={styles.textField} id="standard-basic" label="Login" />
+          <TextField className={styles.textField}
+            id="standard-password-input"
+            label="Password"
+            type="password"
+            autoComplete="current-password"
+          />
+          <Button className={styles.button} variant="outlined">Log in</Button>
+        </form>
       </div>
 
     );

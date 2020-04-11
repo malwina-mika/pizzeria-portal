bb34336a11f8651e2046f1d2c73b562687855882
Malwina8 <48411653+Malwina8@users.noreply.github.com>
2020-04-11 18:00:50 +0200

    Add MUI

--- src/App.js
+++ src/App.js
@@ -14,29 +14,40 @@ import BookingNew from './components/views/Tables/BookingNew';
 import BookingId from './components/views/Tables/BookingId';
 import EventsNew from './components/views/Tables/EventsNew';
 import EventsId from './components/views/Tables/EventsId';
+import { StylesProvider } from '@material-ui/core/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#2B4C6F',
+    },
+  },
+});
 
-function App() {
-  return (
-    <BrowserRouter basename={'/panel'}>
-      <MainLayout>
-        <Switch>
-          <Route exact path={`${process.env.PUBLIC_URL}/`} component={Homepage} />
-          <Route exact path={process.env.PUBLIC_URL + '/login'} component={Login} />
-          <Route exact path={`${process.env.PUBLIC_URL}/tables`} component={Tables} />
-          <Route exact path={`${process.env.PUBLIC_URL}/waiter`} component={Waiter} />
-          <Route exact path={`${process.env.PUBLIC_URL}/kitchen`} component={Kitchen} />
-          <Route exact path={`${process.env.PUBLIC_URL}/waiter/order/new`} component={OrderNew} />
-          <Route exact path={`${process.env.PUBLIC_URL}/waiter/order/:id`} component={OrderId} />
-          <Route exact path={`${process.env.PUBLIC_URL}/tables/booking/new`} component={BookingNew} />
-          <Route exact path={`${process.env.PUBLIC_URL}/tables/booking/:id`} component={BookingId} />
-          <Route exact path={`${process.env.PUBLIC_URL}/tables/events/new`} component={EventsNew} />
-          <Route exact path={`${process.env.PUBLIC_URL}/tables/events/:id`} component={EventsId} />
-
-        </Switch>
-      </MainLayout>
-    </BrowserRouter>
-  );
-}
+const App = () => (
+  <BrowserRouter basename={'/panel'}>
+    <StylesProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <MainLayout>
+          <Switch>
+            <Route exact path={`${process.env.PUBLIC_URL}/`} component={Homepage} />
+            <Route exact path={process.env.PUBLIC_URL + '/login'} component={Login} />
+            <Route exact path={`${process.env.PUBLIC_URL}/tables`} component={Tables} />
+            <Route exact path={`${process.env.PUBLIC_URL}/waiter`} component={Waiter} />
+            <Route exact path={`${process.env.PUBLIC_URL}/kitchen`} component={Kitchen} />
+            <Route exact path={`${process.env.PUBLIC_URL}/waiter/order/new`} component={OrderNew} />
+            <Route exact path={`${process.env.PUBLIC_URL}/waiter/order/:id`} component={OrderId} />
+            <Route exact path={`${process.env.PUBLIC_URL}/tables/booking/new`} component={BookingNew} />
+            <Route exact path={`${process.env.PUBLIC_URL}/tables/booking/:id`} component={BookingId} />
+            <Route exact path={`${process.env.PUBLIC_URL}/tables/events/new`} component={EventsNew} />
+            <Route exact path={`${process.env.PUBLIC_URL}/tables/events/:id`} component={EventsId} />
+          </Switch>
+        </MainLayout>
+      </ThemeProvider>
+    </StylesProvider>
+  </BrowserRouter>
+);
 
 export default App;
